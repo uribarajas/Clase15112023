7fbd0f00cde43a9f53eea39a512dfeb8e434288e
uri barajas <uribarajas1@gmail.com>
2023-11-15 07:23:44 -0600

    primer commit con el archivo app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+const moment = require('moment');
+let i = 5;
+let j = 10;
+console.log('Iniciando');
+console.log('La suma es ' + (5+10));
+
+let fecha = moment().format("LL");
+console.log("Y es la fecha es: ", fecha);
