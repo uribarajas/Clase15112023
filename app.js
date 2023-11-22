a8ed5ff7dbf5ede373504e5df4b9b4776bc0aad3
uri barajas <uribarajas1@gmail.com>
2023-11-22 07:34:43 -0600

    cambios en el archivo yml para examen

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 //const moment = require('moment');
 let i = 5;
 let j = 10;
-console.log('Iniciando');
-console.log('La suma es ' + (5+10));
+//console.log('Iniciando');
+//console.log('La suma es ' + (5+10));
+console.log('Uri Barajas Cicourel');
+console.log('NT720563');
 
 //let fecha = moment().format("LL");
 //console.log("Y es la fecha es: ", fecha);
